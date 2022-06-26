11a25dcbdec574608c0e268f51e2495dab1529ef
你好2007 <2501482523@qq.com>
2022-06-26 15:10:23 +0800

    广播信息发送和接收测试测试成功

--- node.js/ipcMain.on.js
+++ node.js/ipcMain.on.js
@@ -1,4 +1,5 @@
 const { ipcMain, app } = require('electron');
+const dgram = require("dgram");
 
 module.exports = function (win) {
 
@@ -8,5 +9,43 @@ module.exports = function (win) {
     // 退出系统
     ipcMain.on('quit', () => { app.exit(); });
 
+    // 发送信息
+    ipcMain.on('send-msg', function (event, data) {
+        let socket = dgram.createSocket("udp4");
+        socket.bind(function () {
+            socket.setBroadcast(true);
+        });
+        let message = Buffer.from(data);
+        socket.send(message, 0, message.length, 50000, '255.255.255.255', function (err, bytes) {
+            socket.close();
+        });
+
+    });
+
+
+    /**
+     * 监听广播
+     * 监听到来自别的客户端的信息
+     */
+    let server = dgram.createSocket("udp4");
+
+    server.on("error", function (err) {
+        console.log("server error:" + err.stack);
+        server.close();
+    });
+
+    server.on("message", function (msg, rinfo) {
+        // Uint8Array转字符串
+        let dataString = "";
+        for (let i = 0; i < msg.length; i++) {
+            dataString += String.fromCharCode(msg[i]);
+        }
+        win.webContents.send("get-msg", {
+            ip: rinfo.address,
+            msg: dataString
+        });
+    });
+
+    server.bind(50000);
 
 };
--- src/App/index.ts
+++ src/App/index.ts
@@ -9,12 +9,53 @@ import template from './index.html'
 })
 export default class {
 
+    ip: any
+
+    $setup() {
+        return {
+            ip: ref(['192.0.0.1'])
+        }
+    }
+
     quit() {
         globalThis.nodeRequire('electron').ipcRenderer.send('quit')
     }
 
     minimize() {
         globalThis.nodeRequire('electron').ipcRenderer.send('minimize')
+    }
+
+    // 发送信息
+    // target表示接收者的ip，如果是“255.255.255.255”，就是发送给所有的
+    sendMsg() {
+        let msg = {
+            target: "255.255.255.255",
+            data: {
+
+            }
+        }
+        globalThis.nodeRequire('electron').ipcRenderer.send('send-msg', JSON.stringify(msg))
+    }
+
+    $mounted() {
+
+        let WLAN = globalThis.nodeRequire('os').networkInterfaces().WLAN
+        let ip = []
+        for (let index = 0; index < WLAN.length; index++) {
+            if (WLAN[index].family == 'IPv4') {
+                // 虽然mac是唯一的，不过还是ip吧
+                ip.push(WLAN[index].address)
+            }
+        }
+
+        // 启动事件监听主进程
+        globalThis.nodeRequire('electron').ipcRenderer
+
+            // 监听别的软件发送来的信息
+            .on("get-msg", (event, data) => {
+                let msg = JSON.parse(data.msg)
+                console.log(data.ip, msg)
+            })
     }
 
 }
